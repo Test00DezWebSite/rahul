d5caf0209b308477dc4d3b8fdcbfe9f228e05ac7
mohitmv <mohitsaini1196@gmail.com>
2015-07-12 16:57:07 +0530

    First code

---
+++ includes/config.php
@@ -0,0 +1,15 @@
+<?php
+
+$_ginfo["myname"] = "Rahul Saini";
+
+$_ginfo["mysite"] = "www.rahuls.com";
+
+$_ginfo["toplinks"] = array("aboutme" => "About me",
+	"education" => "Education",
+	"projects" => "Projects",
+	"hiring" => "Hiring",
+	"personal" => "Personal",
+	"contactme" => "Contact me"
+	);
+
+?>
